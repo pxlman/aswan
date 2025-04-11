6134c56720d7c9b44bcfb4b4d6f042385926fb52
pxlman <agxgv7573@gmail.com>
2025-04-11 22:42:26 +0200

    [+]

--- aswan.go
+++ aswan.go
@@ -1,7 +1,13 @@
-package aswan
+package main
 
 import (
   "fmt"
 )
 
-fmt.Println("Hello, Aswan!")
+func main() {
+  // This is a simple Go program that prints "Hello, Aswan!" to the console.
+  // It serves as a basic example of how to write a Go program.
+  
+  // Print the greeting message
+  fmt.Println("Hello, Aswan!")
+}
